d453b18a00016c15b9241fd6af0cf0f65ecc9727
manantsoa <manantsoa@wylog.com>
2019-02-27 08:25:10 +0300

    Ajout capture postman pour les services calculerMargeProduit et trierProduitsParOrdreAlphabetique

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
@@ -17,6 +18,7 @@ public class Product {
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
+    @Min(value = 1)
     private int prix;
 
     //information que nous ne souhaitons pas exposer
